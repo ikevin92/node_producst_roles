f509e29d03abbdf63175247f00ec80e72608693e
Kevin Echeverri <ikevin1992@gmail.com>
2021-05-27 00:02:15 -0500

    creacion inical de routes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-const express = require( 'express' );
-
-const app = express();
-const port = 3000;
-
-app.get( '/', ( req, res ) => res.send( 'Hello World!' ) );
+import app from './app';
 
 
-app.listen( port, () => console.log( `Example app listening on port port!` ) );
+const port = 4000;
+
+
+
+app.listen( port );
+console.log( 'expres' );
